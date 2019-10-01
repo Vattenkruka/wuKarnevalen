66fa85137b7afeeb3c966301cda068d84327833a
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-01 17:10:36 +0200

    123

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+
+			// FORM-VALIDATION-FOR-ANSÖK
+const myForm = document.querySelector('#my-form');
+const förnamnInput = document.querySelector('#förnamn');
+const efternamnInput = document.querySelector('#efternamn');
+const emailInput = document.querySelector('#email');
+const telefonInput = document.querySelector('#telefon');
+const sectionInput = document.querySelector('#section');
+const errorElement = document.getElementById('error');
+
+myForm.addEventListener('submit', (e) => {
+	let messages = [];
+	if(förnamnInput.length<1){
+		messages.push('Vänlig skriv in hela ditt förnamn!');
+	}
+	if()
+}
+}
