253a2f6f6ae1ebf470e6a83bd78bfbbda6d1dfff
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-02 14:51:19 +0200

    Revert "12312"
    This reverts commit b6a7aca59367bb60370321aa7ecaec0b4667fe7e.

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
 
 
 			// FORM-VALIDATION-FOR-ANSÖK
-
-			// Form
-const myForm = document.getElementById('my-form')
-// Input fields
-const förnamnInput = document.getElementById('förnamn')
-const efternamnInput = document.getElementById('efternamn')
-const emailInput = document.getElementById('email')
-const telefonInput = document.getElementById('telefon')
-const sectionInput = document.getElementById('section')
-
-
-const errorElement = document.getElementById('error')
+const myForm = document.querySelector('#my-form');
+const förnamnInput = document.querySelector('#förnamn');
+const efternamnInput = document.querySelector('#efternamn');
+const emailInput = document.querySelector('#email');
+const telefonInput = document.querySelector('#telefon');
+const sectionInput = document.querySelector('#section');
+const errorElement = document.getElementById('error');
 
 myForm.addEventListener('submit', (e) => {
-	
-	let messages = []
-	if(förnamnInput.value.length<1){
-		messages.push('Vänlig skriv in hela ditt förnamn!')
+	let messages = [];
+	if(förnamnInput.length<1){
+		messages.push('Vänlig skriv in hela ditt förnamn!');
 	}
-	if(efternamnInput.value.length<1){
-		messages.push('Vänlig skriv in hela ditt efternamn!')
-	}
-	if(emailInput.value.length<1){
-		messages.push('Vänligen skriv in hela din email!')
-	}
-	if(messages.length > 0){
-	e.preventDefault()
-	errorElement.innerText = messages.join(', ')
+	if()
 }
 }
-	
-
