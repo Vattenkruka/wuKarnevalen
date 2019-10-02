85e221ded14b424fc66edd5120c3db2037af7eb5
Simon Arnshed <SimonArnshed@users.noreply.github.com>
2019-10-02 15:43:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,69 +1,9 @@
-
-
-			// FORM-VALIDATION-FOR-ANSÖK
-
-			// Form
-const myForm = document.getElementById('my-form')
-// Input fields
-const förnamnInput = document.getElementById('förnamn')
-const efternamnInput = document.getElementById('efternamn')
-const emailInput = document.getElementById('email')
-const telefonInput = document.getElementById('telefon')
-const sectionInput = document.getElementById('section')
-
-
-const errorElement = document.getElementById('error')
-
-myForm.addEventListener('submit', (e) => {
-	
-	let messages = []
-	if(förnamnInput.value.length<1){
-		messages.push('Vänlig skriv in hela ditt förnamn!')
-	}
-	if(efternamnInput.value.length<1){
-		messages.push('Vänlig skriv in hela ditt efternamn!')
-	}
-	if(emailInput.value.length<1){
-		messages.push('Vänligen skriv in hela din email!')
-	}
-	if(messages.length > 0){
-	e.preventDefault()
-	errorElement.innerText = messages.join(', ')
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
 
-
-let countDown = new Date('Sep 30, 2019 00:00:00').getTime(),
+let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
@@ -73,7 +13,11 @@ let countDown = new Date('Sep 30, 2019 00:00:00').getTime(),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-         }, second)
+      
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
 
-
-
+    }, second)
