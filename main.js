3a21a52da3cc3b77fa8d7650948ed048a889c659
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-02 17:42:49 +0200

    Om du nu vill att jag ska pusha!

--- main.js
+++ main.js
@@ -30,3 +30,18 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+var slideIndex2= 0;
+showSlides2();
+
+function showSlides2() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) {slideIndex2 = 1}
+  slides[slideIndex2-1].style.display = "block";
+  setTimeout(showSlides2, 2000); // Change image every 2 seconds
+}
