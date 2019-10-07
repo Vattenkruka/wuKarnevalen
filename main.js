801a5e46629313196c730f45b9e4454b647016d6
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-07 21:43:25 +0200

    231

--- main.js
+++ main.js
@@ -57,18 +57,203 @@ function commentSubmit(){
 }
 
 function initMap() {
-  var mapOptions = {
-    zoom:16,
-    center: new google.maps.LatLng(55.705, 13.195),
-    mapTypeId: 'roadmap'
-  };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-  var karnevalsPosition = {lat:55.705606,lng: 13.195730};
-  var marker = new google.maps.Marker({
-    position:karnevalsPosition,
-    map: map,
-    title: 'Lundakarnevalen'
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom:13,
+    center:{
+      lat: 55.7055283,
+      lng:13.1925465
+    }
+    
   });
-  
+
+  var flightPlanCoordinates = [{
+          lat: 55.703609,
+          lng: 13.194185
+        },
+        {
+          lat: 55.703639,
+          lng: 13.195355
+        },
+        {
+          lat: 55.703029,
+          lng: 13.196524
+        },
+        {
+          lat: 55.702025,
+          lng: 13.196857
+        },
+        {
+          lat: 55.701983,
+          lng: 13.195119
+        },
+        {
+          lat: 55.702333,
+          lng: 13.192812
+        },
+        {
+          lat: 55.703851,
+          lng: 13.192640
+        },
+        {
+          lat: 55.704214,
+          lng: 13.187823
+        },
+        {
+          lat: 55.706807,
+          lng: 13.187587
+        },
+        {
+          lat: 55.706799,
+          lng: 13.188423
+        },
+        {
+          lat: 55.706190,
+          lng: 13.192569
+        },
+        {
+          lat: 55.709179,
+          lng: 13.192825
+        },
+        {
+          lat: 55.709022,
+          lng: 13.193770
+        },
+        {
+          lat: 55.707910,
+          lng: 13.196409
+        },
+        {
+          lat: 55.707614,
+          lng: 13.196387
+        },
+        {
+          lat: 55.707426,
+          lng: 13.196570
+        },
+        {
+          lat: 55.704894,
+          lng: 13.194853
+        },
+        {
+          lat: 55.703630,
+          lng: 13.194252
+        },
+      ];
+
+      var parade = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: '#FBBCCC',
+        strokeOpacity: 1,
+        strokeWeight: 6
+      });
+
+      
+      var triangleCoords = [{
+          lat: 55.707400,
+          lng: 13.195879
+        },
+        {
+          lat: 55.707327,
+          lng: 13.196351
+        },
+        {
+          lat: 55.706117,
+          lng: 13.195643
+        },
+        {
+          lat: 55.706086,
+          lng: 13.195753
+        },
+        {
+          lat: 55.705754,
+          lng: 13.195539
+        },
+        {
+          lat: 55.705612,
+          lng: 13.196193
+        },
+        {
+          lat: 55.704885,
+          lng: 13.195903
+        },
+        {
+          lat: 55.704365,
+          lng: 13.195480
+        },
+        {
+          lat: 55.704305,
+          lng: 13.195343
+        },
+        {
+          lat: 55.703647,
+          lng: 13.194924
+        },
+        {
+          lat: 55.703623,
+          lng: 13.194122
+        },
+        {
+          lat: 55.703818,
+          lng: 13.192894
+        },
+        {
+          lat: 55.704004,
+          lng: 13.192985
+        },
+        {
+          lat: 55.703797,
+          lng: 13.194045
+        },
+        {
+          lat: 55.703943,
+          lng: 13.194388
+        },
+        {
+          lat: 55.704203,
+          lng: 13.194246
+        },
+        {
+          lat: 55.704502,
+          lng: 13.192713
+        },
+        {
+          lat: 55.705987,
+          lng: 13.192663
+        },
+        {
+          lat: 55.706096,
+          lng: 13.193028
+        },
+        {
+          lat: 55.706111,
+          lng: 13.193840
+        },
+        {
+          lat: 55.706794,
+          lng: 13.193931
+        },
+        {
+          lat: 55.706767,
+          lng: 13.194328
+        },
+        {
+          lat: 55.706571,
+          lng: 13.195266
+        },
+      ];
+
+      var area = new google.maps.Polygon({
+        paths: triangleCoords,
+        strokeColor: '#8A4797',
+        strokeOpacity: 0.6,
+        strokeWeight: 1,
+        fillColor: '#8A4797',
+        fillOpacity: 0.35
+      });
+
+      area.setMap(map);
+
+      parade.setMap(map);
 }
+
