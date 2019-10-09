3bb96fc1e502f71783b01c0df26e017ee45e8cf1
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-09 19:01:24 +0200

    Update FAQ.php

--- FAQ.php
+++ FAQ.php
@@ -102,24 +102,7 @@
 					$db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 					OR die ('No DB-connection via MySQLi');
 
-					echo "<h2>Diskussionsforum</h2>";
-
-					$sql = "SELECT * FROM commentTable";
-
-
-					if ($result=mysqli_query($db_conn,$sql))
-  					{
-    				echo "<table style='border:1px solid #ccc; border-collapse: collapse;'><tr><td style='border:1px solid #ccc'><b>Namn:</b></td><td style='border:1px solid #ccc'><b>Meddelande:</b></td></tr>";
-  					// Fetch one and one row
-  					while ($row=mysqli_fetch_row($result))
-   					 {
-    				printf ("<tr> <td style='border:1px solid #ccc'> %s </td> <td style='border:1px solid #ccc'> %s </td> <tr />\n",$row[0],$row[1]);
-   					 }
-
-    				echo "</table>";
-  					// Free result set
-  					mysqli_free_result($result);
-}
+					
 
 
 					$db_conn->close();
