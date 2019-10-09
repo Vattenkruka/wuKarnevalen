24b762f3eff9a9de4007557204b2138229db41fb
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-09 18:42:07 +0200

    Update FAQ.php

--- FAQ.php
+++ FAQ.php
@@ -93,7 +93,7 @@
 					</aside>
 
 					<?php
-					define ('DB_USER','4003497-db10');
+					define ('DB_USER','4003497_zw91539');
 					define('DB_PASSWORD','REWvoJs6');
 					define('DB_HOST','my06s.sqlserver.se');
 					define('DB_NAME','4003497-db10');
@@ -101,8 +101,6 @@
 
 					$db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 					OR die ('No DB-connection via MySQLi');
-
-					echo "<h2>Diskussionsforum</h2>";
 
 					$db_conn->close();
 
