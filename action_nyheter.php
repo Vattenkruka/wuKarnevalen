a7011d4cdb067eacf55ee87ffd38c2935e5ca70c
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-30 00:31:24 +0100

    9

--- action_nyheter.php
+++ action_nyheter.php
@@ -2,14 +2,14 @@
 require('dbConnection.php');
 $HEADLINE = $_POST["headline"];
 $NMESSAGE =
-$_POST["nMessage"];
+$_POST["nmessage"];
 
 
 $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die ('No DB-connection via MySQLi');
 
 if (!empty($NAME)) {
-	$sql = "INSERT INTO newsTable (headline,nMessage)
+	$sql = "INSERT INTO newsTable (headline,nmessage)
 	VALUES ('$HEADLINE','$NMESSAGE')";
 
 	if ($db_conn->query($sql) === TRUE) {
