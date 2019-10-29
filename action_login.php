45ea1e9c0489ffb43153ea003253b23d67b8ee2e
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-29 21:59:40 +0100

    3123

--- action_login.php
+++ action_login.php
@@ -37,9 +37,7 @@
 				<li id="hantering_av_admin">
 				Hantering av Admin</li>
 				<li> <a href="#Nyhetshantering">Nyhetshantering</a></li>
-				<li> <a href="#Reservationshantering">Reservationshantering</a></li>
-				<li> <a href="#Kommentarshantering">Kommentarshantering</a></li>
-				<li> <a href="#Ansökanhantering">Ansökanhantering</a></li>
+				<li> <a href="#Ansökanhantering">Ansökanhantering</a></li></ol>
 			</div>
 			<div class="admin_cdu_wrapper">
 				<div class="linkh2">
@@ -122,42 +120,17 @@
 				<h2>Adminfunktioner </h2>
 			</div>
 			<ol>
-				<li id="hantering_av_admin">
-				Hantering av Admin</li>
-				<li> <a href="#Nyhetshantering">Nyhetshantering</a></li>
-				<li> <a href="#Reservationshantering">Reservationshantering</a></li>
-				<li> <a href="#Kommentarshantering">Kommentarshantering</a></li>
-				<li> <a href="#Ansökanhantering">Ansökanhantering</a></li>
+				<li> <a href="#hantering_av_admin">
+				Hantering av Admin</a></li>
+				<li id="Nyhetshantering">Nyhetshantering</li>
+				<li> <a href="#Ansökanhantering">Ansökanhantering</a></li></ol>
 			</div>
 			<div class="admin_cdu_wrapper">
 				<div class="linkh2">
-					<h2>Adminverktyg gällande Admins</h2>
+					<h2>Adminverktyg gällande Nyheter</h2>
 				</div>
 				<div  class="adminverktygform">
 					<form action="#" method="POST" id="adminForm">
-						<div class="adminID_field">
-							<label>adminID:*</label>
-							<input type="text" name="adminID"
-							placeholder="Enter adminID" pattern= "[0-9]"  required>
-
-						</div>
-						<div class="username_field">
-							<label>Username:</label>
-							<input type="text" name="adminID"
-							placeholder="Enter username">
-
-						</div>
-						<div class="password_field">
-							<label>Password:</label>
-							<input type="text" name="adminID"
-							placeholder="Enter password">
-						</div>
-						<button input type="button" name="createAdminbtn" id="createAdminbtn" onclick="commentSubmit()" value="Submit"
-						> Skapa Admin</button>
-						<button input type="button" name="commentBtn" id="commentBtnId" onclick="commentSubmit()" value="Submit"
-						> Uppdatera Admin</button>
-						<button input type="button" name="commentBtn" id="commentBtnId" onclick="commentSubmit()" value="Submit"
-						> Ta bort Admin</button>
 					</form>
 					<div class="notificationh4">
 					<h4> Notifikationsfönster</h4>
@@ -169,37 +142,56 @@
 			</div>
 			<div class="admin_read_wrapper">
 			<div class="linkh2">
-			<h2> Adminsök</h2>
+			<h2> Nuvarande Nyhetsflöde</h2>
 			</div>
 			<div class="adminform_wrapper">
-			<form action="#" method="post">
-			<div class="adminID_field">
-							<label>adminID:</label>
-							<input type="text" name="adminID"
-							placeholder="Enter adminID" pattern= "[0-9]">
-
-						</div>
-						<div class="username_field">
-							<label>Username:</label>
-							<input type="text" name="adminID"
-							placeholder="Enter username">
-
-						</div>
-						<div class="password_field">
-							<label>Password:</label>
-							<input type="text" name="adminID"
-							placeholder="Enter password">
-						</div>
-						<button input type="button" name="createAdminbtn" id="createAdminbtn" onclick="commentSubmit()" value="Submit"
-						> Sök Admin!</button>
-						<button input type="button" id="sökAllaAdminsBtn" value="submit"> Sök Alla Admins</button>
-					</form>
-					<div class="adminViewTable">
-					
+			
+					<div class="adminViewTable2">
+					<p>Här ska vi fixa inkludera en nyhetsflöde</p>
 					</div>
 				</div>
 			</div>
+		</div>
+		<div class="KarnevalsAdminH2">
+		<h2> Ansökanhantering</h2>
+		</div>
+		<div class="admin_crud_wrapper">
+		<div class="link_list_wrapper">
+			<div class="linkh2">
+				<h2>Adminfunktioner </h2>
+			</div>
+			<ol>
+				<li> <a href="#hantering_av_admin">
+				Hantering av Admin</a></li>
+				<li> <a href="#Nyhetshantering">Nyhetshantering</a></li>
+				<li id="Ansökanhantering">Ansökanhantering</li></ol>
+			</div>
+			<div class="admin_cdu_wrapper">
+				<div class="linkh2">
+					<h2>Adminverktyg gällande Ansökan</h2>
+				</div>
+				<div  class="adminverktygform">
+					<form action="#" method="POST" id="adminForm">
+					</form>
+					<div class="notificationh4">
+					<h4> Notifikationsfönster</h4>
+					</div>	
+					<div class="admin_messagebox">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum mattis aliquet.  </p>
+					</div>
+				</div>
+			</div>
+			<div class="admin_read_wrapper">
+			<div class="linkh2">
+			<h2> Data över Ansökningar</h2>
+			</div>
+			<div class="adminform_wrapper">
 			
+					<div class="adminViewTable2">
+					<p>Här ska vi inkludera en rolig tabell över ansökningar</p>
+					</div>
+				</div>
+			</div>
 		</div>
 		<footer>
 			<?php include 'footer.php';?>
