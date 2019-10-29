65c487a8a2af44e88d11f5141b717f9305de861d
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-10-30 00:32:23 +0100

    Update action_nyheter.php

--- action_nyheter.php
+++ action_nyheter.php
@@ -8,7 +8,7 @@ $_POST["nmessage"];
 $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die ('No DB-connection via MySQLi');
 
-if (!empty($NAME)) {
+if (!empty($HEADLINE)) {
 	$sql = "INSERT INTO newsTable (headline,nmessage)
 	VALUES ('$HEADLINE','$NMESSAGE')";
 
