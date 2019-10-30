0b37390d5b70992eee3a1859e032cbdd841fbae7
Simon Arnshed <SimonArnshed@users.noreply.github.com>
2019-10-31 00:25:15 +0100

    a

--- action_changeStudentSection.php
+++ action_changeStudentSection.php
@@ -7,20 +7,13 @@ OR die ('No DB-connection via MySQLi');
 
 
 $SECTION = $_POST["section"];
-$STUDENTID = $_POST["studentID"]
+$STUDENTID = $_POST["studentID"];
 
-/*if($_POST["searchAdminBtn"]){
-	$sql = "INSERT INTO adminTable(username, password)
-	VALUES('$USERNAME', '$PASSWORD')";
-	}
-*/
-
-/*if($_POST["searchAllAdminBtn"]){*/
-	if(!empty($STUDENTID)) { 
+if(!empty($STUDENTID)) { 
 	$sql = "UPDATE studentTable
-	SET section='$SECTION',
+	SET section='$SECTION'
 	WHERE studentID='$STUDENTID'";
-}
+
 
 
 	if ($db_conn->query($sql) === TRUE) {
@@ -28,21 +21,8 @@ $STUDENTID = $_POST["studentID"]
 	} else {
 		echo "Fel: " . $sql . "<br>" . $db_conn->error;
 	}
-
-		
-
-		
-/*}*/
-/*if ($db_conn->query($sql) === TRUE) {
-		echo "<br />Din biljett är reserverad min gode herre<br /><br />";
-	} else {
-		echo "Fel: " . $sql . "<br>" . 
-		$db_conn->error;
-	}
-
-
 }
-*/
+
 
 
 
