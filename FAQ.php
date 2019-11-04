bb9b96a78d77503b293b067658ee732f7f7f76af
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2019-11-04 01:13:57 +0100

    FAQ-fix

--- FAQ.php
+++ FAQ.php
@@ -20,31 +20,37 @@
 				<h1> Frequently Asked Questions</h1>
 				<div class="Nöjenwrapper">
 					<article class="nöje1">
-						<h2> Fråga 1? </h2>
-						<p>Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2> När är Karnevalen? </h2>
+						<p>Nästa Lundakarneval är år 2022.
 						</p>
-						<h2> Fråga 3? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2>Kostar det något att titta på Karnelvalståget?</h2>
+						<p> 
+						Nej, Karnevalståget är helt gratis och kan ses från valfri plats enligt kartan över tågets rutt! 
 						</p>
-						<h2> Fråga 5? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2> När och var kan jag titta på Karnevalståget</h2>
+						<p> Du kan se tåget lördagen den 19 maj och söndagen den 20 maj. Tåget startar 13:00 och du kan titta på tåget från någon av alla de platser som finns längs med tågets rutt. Se karta för rutten under fliken ”Om Karnevalen” --> ”Tåg”.
 						</p>
-						<h2> Fråga 7? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2> Vilka artister kommer på Lundakarnevalen 2022?</h2>
+						<p> Lundakarnevalen 2022 kommer att få besök av några av världens bästa artister: Michael Jackson Hologram, Phil Collins, Simon Da Rapper och Dj Ogge MCBeatboxer!
 						</p>
 
 					</article>
 					<article class ="nöje2">
-						<h2> Fråga 2? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.</p>
-						<h2> Fråga 4? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2> Var kan jag parkera min cykel på Lundakarnevalen?
+ 						</h2>
+						<p> Helst i de officiella cykelställ som finns lite överallt i Lund. Men akta er för att parkera cykeln bredvid ett cykelställ utanför ICA Clementstorget, då kommer polisen och tar den efter 5 röda sekunder!
 						</p>
-						<h2> Fråga 6? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2> Vad kan jag göra av min väska och värdesaker?
+						 </h2>
+						<p> Lundakarnevalen ansvarar inte för besökares värdesaker. Vi rekommenderar att ni, som på andra festivaler/karnevaler bara tar med det mest nödvändiga, men om ni prompt vill att någon ska ta hand om era värdesaker så rekommenderar vi ficktjuvarna som stryker omkring. De står så gärna till er tjänst!
 						</p>
-						<h2> Fråga 8? </h2>
-						<p> Nulla consectetur, nunc eu sodales congue, risus risus dignissim nulla, quis venenatis augue nisi molestie elit. Sed venenatis porttitor ultricies. Nunc ante arcu, efficitur eu scelerisque maximus, aliquam at magna. Maecenas sit amet lorem ac odio eleifend iaculis nec ac purus. Duis vel nulla fringilla, convallis purus quis, condimentum nisl.
+						<h2> Hur kommer kollektivtrafiken att fungera under Lundakarnevalen?
+						</h2>
+						<p> Troligtvis lika uselt som vanligt.
+						</p>
+						<h2> Hur har ni ordnat för små barn på festivalen?
+						</h2>
+						<p> Det finns alltid roliga barnaktiviteter som håller på främst under dagarna. Vi rekommenderar dock att alla barn och unga mellan 0 till 12 år eskorteras av en vuxen. Vad gäller små människor så står Lundakarnevalen inte för eventuell begränsad sikt under konserter, men borttappade korta vänner kan hämtas upp vid huvudingången för Karnevalsområdet! 
 						</p>
 
 					</article>			
